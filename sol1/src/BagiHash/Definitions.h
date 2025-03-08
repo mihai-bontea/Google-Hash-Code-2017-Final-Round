94c4bd610f1f2f891eda9bc78be102091a5fe706
mihai-bontea <mihaibontea@protonmail.com>
2025-03-08 13:52:24 +0200

    folder restructurin

--- sol1/src/BagiHash/Definitions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-using Matrix = std::pair < std::pair<unsigned int, unsigned int>, std::pair<unsigned int, unsigned int>>;
-using query_result = std::pair<unsigned int, std::pair<unsigned int, unsigned int>>;
-using Point = pair<unsigned int, unsigned int>;
